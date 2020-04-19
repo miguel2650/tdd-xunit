4036dfb26bbb22408bc63c724875d80db2a0a7a9
Mikkel Madsen <slaske.madsen@gmail.com>
2020-04-19 17:39:46 +0200

    Added more user input and more test cases. New TODO added on line 3 @ PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,5 +1,6 @@
 class PasswordGenerator():
 
+    # TODO: IF ALL IS FALSE WHAT IS PASSWORD? Length of X and contain ????
     def __init__(self, length=10, characters=True, numbers=True, specialChar=True, uppercase=True, lowercase=True):
         self.length = length
         self.characters = characters
@@ -18,14 +19,52 @@ class PasswordGenerator():
             raise Exception("Please use a number as length!")
         self._length = len
 
+    @length.getter
+    def length(self):
+        return self._length
+
+    # Function generate password based on user settings.
+    def generatePassword(self):
+        pass
+
     def __str__(self):
         return str(self.length)
 
 
 if __name__ == "__main__":
+    validInput = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
     print("Welcome to password generator")
     print("Answer the questions below to generate a new password")
     print("How long would you like your password to be?")
     inputLength = int(input())
-    passwordGen = PasswordGenerator(inputLength)
-    print('new password is ', passwordGen)
+    print("Would you like your password to contain characters? yes/no")
+    inputCharacters = str(input())
+    if(inputCharacters.lower() not in validInput):
+        raise ValueError("Invalid answer")
+    print("Would you like your password to contain numbers? yes/no")
+    inputNumbers = str(input())
+    if(inputNumbers.lower() not in validInput):
+        raise ValueError("Invalid answer")
+    print("Would you like your password to contain special characters? yes/no")
+    inputSpecialChar = str(input())
+    if(inputSpecialChar.lower() not in validInput):
+        raise ValueError("Invalid answer")
+    print("Would you like your password to contain uppercase letters? yes/no")
+    inputUppercase = str(input())
+    if(inputUppercase.lower() not in validInput):
+        raise ValueError("Invalid answer")
+    print("Would you like your password to contain lowercase letters? yes/no")
+    inputLowercase = str(input())
+    if(inputLowercase.lower() not in validInput):
+        raise ValueError("Invalid answer")
+
+    passwordGen = PasswordGenerator(
+        inputLength, validInput[inputCharacters.lower()], validInput[inputNumbers.lower()], validInput[inputSpecialChar.lower()], validInput[inputUppercase.lower()], validInput[inputLowercase.lower()])
+
+    print('Success: Your new password will be created with following settings...')
+    print("Password length:", passwordGen.length)
+    print("Contain characters:", passwordGen.characters)
+    print("Contain numbers:", passwordGen.numbers)
+    print("Contain special characters:", passwordGen.specialChar)
+    print("Contain uppercase letters:", passwordGen.uppercase)
+    print("Contain lowercase letters:", passwordGen.lowercase)
--- TestPasswordGenerator.py
+++ TestPasswordGenerator.py
@@ -5,16 +5,23 @@ from PasswordGenerator import PasswordGenerator
 class TestPasswordGenerator(unittest.TestCase):
 
     def test_length(self):
-        # Instance of PasswordGenerator class
+        # Instance of PasswordGenerator class with default values
+        # length=10, characters=True, numbers=True, specialChar=True, uppercase=True, lowercase=True
         passwordGen = PasswordGenerator()
 
         # Test cases for expected exceptions.
-        testCases = [0.1, "string", None]
-        # Tests that must fail
-        for testcase in testCases:
+        failTestCases = [0.1, "string", None]
+        for testcase in failTestCases:
             print("Expect exception with a", type(testcase))
             with self.assertRaises(Exception):
                 passwordGen.length = testcase
+
+        # Test cases for expected success.
+        successTestCases = [-1000, -1, 0, 1, 1000]
+        for testcase in successTestCases:
+            print("Expect success with ", testcase)
+            passwordGen.length = testcase
+            self.assertEqual(testcase, passwordGen.length)
 
 
 if __name__ == "__main__":
