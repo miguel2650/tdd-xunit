0501307775387ca6eff173117fd43d706d6d418a
Mikkel Madsen <slaske.madsen@gmail.com>
2020-04-19 15:42:34 +0200

    Added more test cases for 'setter.length' function

--- TestPasswordGenerator.py
+++ TestPasswordGenerator.py
@@ -8,9 +8,13 @@ class TestPasswordGenerator(unittest.TestCase):
         # Instance of PasswordGenerator class
         passwordGen = PasswordGenerator()
 
+        # Test cases for expected exceptions.
+        testCases = [0.1, "string", None]
         # Tests that must fail
-        with self.assertRaises(Exception):
-            passwordGen.length = "i am a string"
+        for testcase in testCases:
+            print("Expect exception with a", type(testcase))
+            with self.assertRaises(Exception):
+                passwordGen.length = testcase
 
 
 if __name__ == "__main__":
